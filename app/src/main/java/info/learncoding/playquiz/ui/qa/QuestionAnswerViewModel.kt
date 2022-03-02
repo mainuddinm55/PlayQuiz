87c24fec3dc60cfadc496a5aa4a8a0f0b55ed81b
Md Mainuddin <mainuddinm55@gmail.com>
2022-03-03 01:18:11 +0600

    cancel count down timer after timer finish

--- app/src/main/java/info/learncoding/playquiz/ui/qa/QuestionAnswerViewModel.kt
+++ app/src/main/java/info/learncoding/playquiz/ui/qa/QuestionAnswerViewModel.kt
@@ -149,6 +149,7 @@ class QuestionAnswerViewModel @Inject constructor(
             }
 
             override fun onFinish() {
+                countDownTimer?.cancel()
                 updateNextQuestion()
             }
         }.start()
